6302364c6cb7428e6d96aa8d3eb1e7c0f64bdd21
Whoami <nurlanguliev33@gmail.com>
2024-03-27 20:56:58 -0700

    Java2

---
+++ app1.js
@@ -0,0 +1,75 @@
+// 1 daxil olunan ededin sade yoxsa murekkeb oldugunu tapin
+// let bir = +prompt('eded');
+// for(let i = 1;i<=bir; i++){
+//     if(bir<2){
+// console.log('sade eded deyil');
+// break;
+//     }
+//     if((bir%i === 0 && i !==1 && i !== bir )){
+//         console.log("sade eded deyil");
+//         break;
+//     }
+//     if(bir===i){
+//         console.log("sade ededdir");
+//     }
+// }
+
+
+
+
+// 2 Daxil olunan ədədin mükəmməl olub olmamagi
+// let num2 = +prompt('eded')
+// let sum = 0
+
+// for(let i = 1; i < num2; i++){
+//     if(num2 % i === 0){
+//         sum = sum + i
+//     }
+// }
+
+// if(sum === num2){
+//     console.log('Mukemmeldi');
+// } else {
+//     console.log('Mukemmel deyil');
+// }
+
+
+
+
+// 3. Daxil olunan 2 ədəd arasındakı mükəmməl ədədlər
+// let bir = +prompt('Eded')
+// let iki = +prompt('Eded2')
+
+// for (let eded = bir; eded <= iki; eded++) {
+//     let sum = 0
+
+//     for (let i = 1; i < eded; i++) {
+//         if (eded % i === 0) {
+//             sum = sum + i
+//         }
+//     }
+
+//     if (sum === eded) {
+//         console.log(eded);
+//     }
+// }
+
+
+
+//4. 2 eded daxil edir, aradaki sade ededler
+// let eded1 = +prompt("Ededi1 daxil edin: ");
+// let eded2 = +prompt("Ededi2 daxil edin: ");
+
+// for (let j = eded1; j <= eded2; j++) {
+//   for (let i = 1; i <= j; i++) {
+//     if (j % i === 0 && i !== 1) {
+//       if (j > i) {
+//         break;
+//       }
+//       if (j === i) {
+//         console.log(i);
+//       }
+//     }
+//   }
+// }
+
